7c68dac8854a77e72aeea33b01a8280d3d4918e3
Touseef Elahi <touseefelahi@ymail.com>
2022-10-19 17:53:00 +0300

    Fix test dependency

--- GeneralPacketSender/PacketSender.CoreTests/Services/UdpTransceiverTests.cs
+++ GeneralPacketSender/PacketSender.CoreTests/Services/UdpTransceiverTests.cs
@@ -1,19 +1,20 @@
-﻿using Xunit;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PacketSender.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xunit;
 namespace PacketSender.Core.Tests
 {
+    [TestClass]
     public class UdpTransceiverTests
     {
-        [Fact()]
+        [TestMethod]
         public async Task SendAsyncTest()
         {
-            UdpTransceiver udpTransceiver = new("192.168.250.105", 4454, 500);
+            UdpTransceiver udpTransceiver = new("192.168.250.105", 4454);
             var bytes = new byte[] { 5, 4, 3, 2, 1 };
             var reply = await udpTransceiver.SendAsync(bytes, true);
         }
